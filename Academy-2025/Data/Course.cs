af75ec1ac389368a14e798f7761637ec91c888f8
Horváth Olivér <horvatholiver0929@gmail.com>
2025-02-24 11:34:30 +0100

    CourseController, NuGet

---
+++ Academy-2025/Data/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace Academy_2025.Data
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Calculator-Academy-2025/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator_Academy_2025
+{
+    public class Calculator
+    {
+        public float Addition(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float Subtraction(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Multiplication(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Division(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
